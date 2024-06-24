2bc89c2d1685edfc10870ed7eb0a6d4f15773aad
Alejandro Rojo <pinkyig@MacBook-Pro-de-Alejandro.local>
2024-06-24 00:19:00 -0400

    [Update] Se acutalizaon los comentarios explicativos para los predicados.

--- p3.pl
+++ p3.pl
@@ -1,4 +1,5 @@
 % Definición de todos los  caminos en el laberinto.
+% Formato -> camino(Origen, Destino).
 camino(p11, p12).
 camino(p12, p13).
 camino(p13, p14).
@@ -17,13 +18,24 @@ camino(p34, m).
 %Caso en que el nodo de inicio sea m
 alcanzable(m, m, _) :- !.
 
-
+%Predicado auxiliar para verificar si se puede llegar a la meta desde un nodo dado.
 alcanzable(Origen, Destino, Visitados) :-
+    % Origen: nodo de inicio
+    % Destino: nodo de destino
+    % Visitados: lista de nodos visitados
+
+    %el predicado alcanzable recibe un nodo de inicio, un nodo de destino y una lista de nodos visitados 
+    %y verifica si se puede llegar al nodo de destino desde el nodo de inicio sin pasar por nodos ya visitados,
+    %si se puede llegar al nodo de destino desde el nodo de inicio sin pasar por nodos ya visitados, el predicado es verdadero
+    %si no se puede llegar al nodo de destino desde el nodo de inicio sin pasar por nodos ya visitados, el predicado es falso
+    
+    
     camino(Origen, Siguiente),
     \+ member(Siguiente, Visitados), % Verifica que no se haya visitado.
     alcanzable(Siguiente, Destino, [Siguiente|Visitados]). % Continúa la búsqueda.
 
-%caso en que inico sea vacio escribir no hay nodo de inicio
+
+%caso en que inico sea vacio, por lo que devuelve un mensaje de error
 verificar() :-
     write('No hay nodo de inicio').
 
@@ -31,4 +43,9 @@ verificar() :-
 
 % Predicado principal para verificar si se puede llegar a la meta desde un nodo dado.
 verificar(Inicio) :-
+    % Inicio: nodo de inicio
+
+    %el predicado verificar recibe un nodo de inicio y llama al predicado auxiliar alcanzable 
+    %para verificar si se puede llegar a la meta desde el nodo dado como inicio
+
     alcanzable(Inicio, m, [Inicio]).
